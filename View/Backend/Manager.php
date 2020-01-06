0babae870c96bc12849f2a75482d3c9fcb7c3b4d
Miguel-Metmer <Miguel.Metmer@outlook.com>
2020-01-06 21:54:45 +0100

    Correction

--- View/Backend/Manager.php
+++ View/Backend/Manager.php
@@ -14,3 +14,4 @@ class Manager
         return $bdd;
     }
 }
+?>
--- index.php
+++ index.php
@@ -1,92 +1,92 @@
 <?php
-    require("Controller/Controller.php");
-    if(isset($_GET["action"]))
+require("Controller/Controller.php");
+if(isset($_GET["action"]))
+{
+    if($_GET["action"] == "Register_New_User")
     {
-        if($_GET["action"] == "Register_New_User")
-        {
-            Register_New_User();
-        }
-        elseif($_GET["action"] == "Connect_User")
-        {
-            Connect_User();
-        }
-        elseif($_GET["action"] == "Logout")
-        {
-            Disconnect();
-        }
-        elseif($_GET["action"] == "Create_Article")
-        {
-            Create_Article();
-        }
-        elseif($_GET["action"] == "Delete_Article")
-        {
-            Delete_Article($_POST['articles']);
-        }
-        elseif($_GET["action"] == "Get_Article")
-        {
-            Get_Article($_GET['Id']);
-        }
-        elseif($_GET["action"] == "Get_Articles")
-        {
-            Get_Articles();
-        }
-        elseif($_GET["action"] == "Show_Informations")
-        {
-            Show_Informations();
-        }
-        elseif($_GET["action"] == "To_Connect")
-        {
-            To_Connect();
-        }
-        elseif($_GET["action"] == "To_Register")
-        {
-            To_Register();
-        }
-        elseif($_GET["action"] == "Set_Comment")
-        {
-            Set_Comment($_GET["Id"]);
-        }
-        elseif($_GET["action"] == "Modify_Article")
-        {
-            Modify_Article();
-        }
-        elseif($_GET["action"] == "Get_Mentions")
-        {
-            Get_Mentions();
-        }
-        elseif($_GET["action"] == "Report_Comment")
-        {
-            Report_Comment($_GET["News_Owner"], $_GET["Comment_Id"]);
-        }
-        elseif($_GET["action"] == "Get_Reported_Comments")
-        {
-            Get_Reported_Comments();
-        }
-        else
-        {
-            echo "Error";
-        }
+        Register_New_User();
     }
-    elseif (isset($_POST["Delete_Article"])) 
+    elseif($_GET["action"] == "Connect_User")
     {
-        Delete_Article();
+        Connect_User();
     }
-    elseif (isset($_POST["Delete_Comment"])) 
+    elseif($_GET["action"] == "Logout")
     {
-        Delete_Comment();
+        Disconnect();
     }
-    elseif (isset($_POST["Authorize_Comment"])) 
+    elseif($_GET["action"] == "Create_Article")
     {
-        Authorize_Comment();
+        Create_Article();
     }
-    elseif (isset($_POST["Edit"])) 
+    elseif($_GET["action"] == "Delete_Article")
     {
-        Edit_Article();
+        Delete_Article($_POST['articles']);
+    }
+    elseif($_GET["action"] == "Get_Article")
+    {
+        Get_Article($_GET['Id']);
+    }
+    elseif($_GET["action"] == "Get_Articles")
+    {
+        Get_Articles();
+    }
+    elseif($_GET["action"] == "Show_Informations")
+    {
+        Show_Informations();
+    }
+    elseif($_GET["action"] == "To_Connect")
+    {
+        To_Connect();
+    }
+    elseif($_GET["action"] == "To_Register")
+    {
+        To_Register();
+    }
+    elseif($_GET["action"] == "Set_Comment")
+    {
+        Set_Comment($_GET["Id"]);
+    }
+    elseif($_GET["action"] == "Modify_Article")
+    {
+        Modify_Article();
+    }
+    elseif($_GET["action"] == "Get_Mentions")
+    {
+        Get_Mentions();
+    }
+    elseif($_GET["action"] == "Report_Comment")
+    {
+        Report_Comment($_GET["News_Owner"], $_GET["Comment_Id"]);
+    }
+    elseif($_GET["action"] == "Get_Reported_Comments")
+    {
+        Get_Reported_Comments();
     }
     else
     {
-        Show_Articles();
+        echo "Error";
     }
+}
+elseif (isset($_POST["Delete_Article"])) 
+{
+    Delete_Article();
+}
+elseif (isset($_POST["Delete_Comment"])) 
+{
+    Delete_Comment();
+}
+elseif (isset($_POST["Authorize_Comment"])) 
+{
+    Authorize_Comment();
+}
+elseif (isset($_POST["Edit"])) 
+{
+    Edit_Article();
+}
+else
+{
+    Show_Articles();
+}
 
 
 
