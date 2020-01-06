470c247a9801eea2b6d08bd4a7598ce451218fc8
Miguel-Metmer <Miguel.Metmer@outlook.com>
2020-01-06 20:25:09 +0100

    Ajout d'un fichier Manager.php

---
+++ View/Backend/Manager.php
@@ -0,0 +1,18 @@
+<?php
+    class Manager
+    {
+        protected function connect_To_Database()
+        {
+            //On se connecte à la base de données
+            try
+            {
+                $this->bdd = new PDO('mysql:host=localhost;dbname=maBase;charset=utf8', 'root', 'Miguel', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            }
+            catch(Exception $e)
+            {
+                die('Erreur : ' . $e->getmessage()); // On renvoie un message si il y a une erreur.
+            }
+
+            return $this->bdd;
+        }
+    }
