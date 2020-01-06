f14e6e3afc34f2e43cd9afdb1cbf635e164a3903
Miguel-Metmer <Miguel.Metmer@outlook.com>
2020-01-06 22:05:40 +0100

    Code fonctionnel

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 require("Controller/Controller.php");
-<<<<<<< HEAD
-//Je mets ça là.
-=======
->>>>>>> modification
 if(isset($_GET["action"]))
 {
     if($_GET["action"] == "Register_New_User")
@@ -29,7 +25,6 @@ if(isset($_GET["action"]))
     elseif($_GET["action"] == "Get_Article")
     {
         Get_Article($_GET['Id']);
-<<<<<<< HEAD
     }
     elseif($_GET["action"] == "Get_Articles")
     {
@@ -61,42 +56,6 @@ if(isset($_GET["action"]))
     }
     elseif($_GET["action"] == "Report_Comment")
     {
-        /*$test = $_GET['News_Owner'];
-        echo '<script>alert('. $test .')</script>'; */
-
-=======
-    }
-    elseif($_GET["action"] == "Get_Articles")
-    {
-        Get_Articles();
-    }
-    elseif($_GET["action"] == "Show_Informations")
-    {
-        Show_Informations();
-    }
-    elseif($_GET["action"] == "To_Connect")
-    {
-        To_Connect();
-    }
-    elseif($_GET["action"] == "To_Register")
-    {
-        To_Register();
-    }
-    elseif($_GET["action"] == "Set_Comment")
-    {
-        Set_Comment($_GET["Id"]);
-    }
-    elseif($_GET["action"] == "Modify_Article")
-    {
-        Modify_Article();
-    }
-    elseif($_GET["action"] == "Get_Mentions")
-    {
-        Get_Mentions();
-    }
-    elseif($_GET["action"] == "Report_Comment")
-    {
->>>>>>> modification
         Report_Comment($_GET["News_Owner"], $_GET["Comment_Id"]);
     }
     elseif($_GET["action"] == "Get_Reported_Comments")
